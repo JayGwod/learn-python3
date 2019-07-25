7a12d7b39fdf5c5a65211b828907ed03f5c0024f
JayGwod <guodejie001@126.com>
2019-07-25 18:22:27 +0800

    Create hello_gui.py

---
+++ exemples/hello_gui.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 GUO DEJIE <dejie.guo@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+使用Tkinter十分简单，我们来编写一个GUI版本的“Hello, world!”。
+"""
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+
+app = Application()
+# 设置窗口标题：
+app.master.title("Hello World")
+# 主消息循环：
+app.mainloop()
